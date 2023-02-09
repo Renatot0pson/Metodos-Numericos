130acea37476d508123725264c4925bad7378807
Renato Fernandes <85365141+Renatot0pson@users.noreply.github.com>
2023-02-09 11:49:54 -0400

    Update vendas.js

--- vendas.js
+++ vendas.js
@@ -1,3 +1,4 @@
+
 let renato01 = parseFloat(prompt('vendas 1'))
 let renato02 = parseFloat(prompt('vendas 2'))
 let renato03 = parseFloat(prompt('vendas 3'))
@@ -13,4 +14,26 @@ if(vendas <= 100){
 }
 
 comissao = percentual * vendas
-alert('O total de suas vendas foi: ' + percentual + ' e sua comissão é: ' + comissao)
+console.log(`O percentual é ${percentual * 100}% e a comissao é {comissao}`)
+
+----------------------------------------------------------------------------------------
+/*METEDO USANDO FOREACH E ARRAY*/
+
+var vendas = [10, 15, 30]
+var totalVendas = 0
+var comissao = 0
+
+vendas.forEach(function(value, index){
+  totalVendas = totalVendas + value
+})
+
+  if(totalVendas <= 100){
+    comissao = totalVendas * 0.1
+  }else if (totalVendas > 100 && totalVendas <= 500){
+    comissao = totalVendas * 0.2
+  }else if(totalVendas > 500){
+    comissao = totalVendas * 0.3
+  }
+
+alert('Total de suas vendas foi: ' + totalVendas)
+alert('Sua comissão foi ' + comissao)
